1b5767721ecc6a191d8f8b435126d34d6e75d0a5
Andrey Mart'yanov <v.osnovnom.bezvreden@gmail.com>
2017-02-27 06:18:06 +0300

    вычистил ассеты с кривыми именами

---
+++ Assets/Test5.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Test5 : MonoBehaviour {
+
+
+	public GameObject myObject;
+	public SpriteRenderer myComponent;
+
+	public GameObject anotherObject;
+	public SpriteRenderer anotherComponent;
+
+	public Sprite newSprite;
+
+	// Use this for initialization
+	void Start () {
+
+
+
+		myObject= GameObject.Find ("Test5");
+		myComponent = myObject.GetComponent<SpriteRenderer> ();
+
+
+		anotherObject= GameObject.Find ("Test6");
+		anotherComponent = anotherObject.GetComponent<SpriteRenderer> ();
+
+
+		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+		Debug.Log ("anotherComponent.sprite is:" + anotherComponent.sprite);
+//		newSprite = (Sprite) Resources.Load <Sprite> ("table");
+		myComponent.sprite = anotherComponent.sprite;
+
+		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+		Debug.Log ("anotherComponent.sprite is:" + anotherComponent.sprite);
+
+//		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+//		Debug.Log ("myObject is:" + myObject);
+//		Debug.Log ("myComponent is:" + myComponent);
+
+//		Debug.Log ("anotherObject is:" + anotherObject);
+//		Debug.Log ("anotherComponent is:" + anotherComponent);
+
+//		Debug.Log ("newSprite is:" + newSprite);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		myComponent.sprite = anotherComponent.sprite;
+	}
+}
