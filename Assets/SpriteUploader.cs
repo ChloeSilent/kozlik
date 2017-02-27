5dd8c3a5b838cfdad36f2c9982b4ddd6baeb668c
Andrey Mart'yanov <v.osnovnom.bezvreden@gmail.com>
2017-02-27 09:23:12 +0300

    проебавшись ночь научился аплоудить спрайт кодом и помещать результат в компонент

---
+++ Assets/SpriteUploader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpriteUploader : MonoBehaviour {
+
+	public GameObject myObject;
+//	public Component myComponent;
+	public Sprite newSprite;
+	public Sprite currentSprite;
+
+	// Use this for initialization
+	void Start () {
+		myObject= GameObject.Find ("Button1");
+		currentSprite = myObject.GetComponent<Image> ().sprite;
+
+		newSprite = (Sprite) Resources.Load <Sprite> ("object001");
+
+		myObject.GetComponent<Image> ().sprite = newSprite;
+//		currentSprite = newSprite;
+
+
+		//		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+		//		Debug.Log ("anotherComponent.sprite is:" + anotherComponent.sprite);
+		//		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+		//		Debug.Log ("anotherComponent.sprite is:" + anotherComponent.sprite);
+		//		Debug.Log ("myComponent.sprite is:" + myComponent.sprite);
+				Debug.Log ("myObject is:" + myObject);
+		//		Debug.Log ("myComponent is:" + myComponent);
+		//		Debug.Log ("anotherObject is:" + anotherObject);
+		//		Debug.Log ("anotherComponent is:" + anotherComponent);
+				Debug.Log ("newSprite is:" + newSprite);
+				Debug.Log ("currentSprite is:" + currentSprite);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		currentSprite = newSprite;
+//		Debug.Log ("NEW currentSprite is:" + currentSprite);
+	}
+}
--- Assets/Test5.cs
+++ Assets/Test5.cs
@@ -19,7 +19,7 @@ public class Test5 : MonoBehaviour {
 
 
 
-		myObject= GameObject.Find ("Test5");
+		myObject= GameObject.Find ("Button (1)");
 		myComponent = myObject.GetComponent<SpriteRenderer> ();
 
 
