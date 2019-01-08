c50a7466596e91d38a7704bfcb8b77b2eb4697e1
Andrey Martianov <v-osnovnom-bezvreden@yandex.ru>
2019-01-08 14:05:58 +0300

    all line endings corrected to win. close #16

--- Assets/Scripts/ButtonsController.cs
+++ Assets/Scripts/ButtonsController.cs
@@ -1,1 +1,118 @@
-﻿using System.Collections;using System.Collections.Generic; // нужно для  [System.Serializable]using UnityEngine;using UnityEngine.UI;using System.Runtime.CompilerServices; //что это и зачем ?public class ButtonsController : MonoBehaviour {	public List<Item> currentItemList; //здесь хранится отфильтрованный из allItemsList контент 	private ButtonsController currentController;	public SimpleObjectPool buttonObjectPool;	public ButtonsController objectPickerButtonsController;	public ButtonsController categoryPickerButtonsController;	public OrganizeData dataContainer;	//взять кнопку из пула и просетапить ее полученным аргументом itemToSetupWith	public void TakeOneButtonFromPoolAndSetupWith(Item itemToSetupWith)	{		currentController = this;  // а почему сразу не использовать this ?        //для правильного скейлинга при извлечении из пула нужно утанавливать parent. для этого передаем currentController        GameObject newButton = buttonObjectPool.GetObject (currentController.transform);		SampleButton sampleButton = newButton.GetComponent<SampleButton> ();		sampleButton.Setup (itemToSetupWith);		//смена парента при возвращении из пула  ломает не только скейлинг, но и якоря и оффсеты. жестко фиксим  		RectTransform buttonRectTransform = sampleButton.GetComponent<RectTransform>();		buttonRectTransform.anchorMax = new Vector2(1, 1);		buttonRectTransform.anchorMin = new Vector2(0, 0);		buttonRectTransform.offsetMin = new Vector2(buttonRectTransform.offsetMin.x, 0);		buttonRectTransform.offsetMax = new Vector2(buttonRectTransform.offsetMax.x, 0);		buttonRectTransform.offsetMin = new Vector2(buttonRectTransform.offsetMin.y, 0);		buttonRectTransform.offsetMax = new Vector2(buttonRectTransform.offsetMax.y, 0);	}	//сетапим все кнопки из currentItemList в цикле	public void AddButtonsFromCurrentItemList()	{		for (int i = 0; i < currentItemList.Count; i++) 		{			Item currentItem = currentItemList [i];			TakeOneButtonFromPoolAndSetupWith (currentItem);		}	}	public void TuneButtonsForMain ()	{        foreach (Transform mainButton in transform) 		{			mainButton.GetComponent <SampleButton> ().TuneButtonForMain ();		}	}	public void TuneButtonsForBrowse ()	{        foreach (Transform browseButton in transform) 		{			browseButton.GetComponent <SampleButton> ().TuneButtonForBrowse ();        }	}	public void TuneButtonsForQuiz ()	{		foreach (Transform quizButton in transform) 		{			quizButton.GetComponent <SampleButton> ().TuneButtonForQuiz ();		}	}    // из общего списка объектов отфильтруем тех, кто не объект, а категория объектов    public void FilterCategoryPickerItemList()	{		foreach (Item sortedItem in dataContainer.allItemsList) 		{			if (sortedItem.isACategory == true) 			{ 				categoryPickerButtonsController.currentItemList.Add (sortedItem);			}		}	}    // из общего списка объектов отфильтруем тех, кто принадлежит нужной категории    public void FilterObjectPickerItemListTo (int desiredCategoryId)	{		foreach (Item sortedItem in dataContainer.allItemsList)		{			if (sortedItem.Category == desiredCategoryId &&  sortedItem.isACategory ==false)			{				objectPickerButtonsController.currentItemList.Add (sortedItem);			}		}	}	//убрать в пул одну кнопку 	public void ReturnOneButtonToPool ()	{			GameObject toBeRemoved = transform.GetChild (0).gameObject;		buttonObjectPool.ReturnObject (toBeRemoved);	}	//убирать в пул все кнопки  пока не кончатся	public void RemoveAllButtons() 	{		currentController = this;		while (currentController.transform.childCount > 0)        // а почему сразу не использовать this ?        // TODO разобраться        {            ReturnOneButtonToPool ();		}	}}
+using System.Collections;
+using System.Collections.Generic; // нужно для  [System.Serializable]
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.CompilerServices; //что это и зачем ?
+
+public class ButtonsController : MonoBehaviour 
+{
+	public List<Item> currentItemList; //здесь хранится отфильтрованный из allItemsList контент 
+	private ButtonsController currentController;
+	public SimpleObjectPool buttonObjectPool;
+
+	public ButtonsController objectPickerButtonsController;
+	public ButtonsController categoryPickerButtonsController;
+
+	public OrganizeData dataContainer;
+
+
+	//взять кнопку из пула и просетапить ее полученным аргументом itemToSetupWith
+	public void TakeOneButtonFromPoolAndSetupWith(Item itemToSetupWith)
+	{
+		currentController = this;  // а почему сразу не использовать this ?
+
+        //для правильного скейлинга при извлечении из пула нужно утанавливать parent. для этого передаем currentController
+        GameObject newButton = buttonObjectPool.GetObject (currentController.transform);
+		SampleButton sampleButton = newButton.GetComponent<SampleButton> ();
+		sampleButton.Setup (itemToSetupWith);
+
+		//смена парента при возвращении из пула  ломает не только скейлинг, но и якоря и оффсеты. жестко фиксим  
+		RectTransform buttonRectTransform = sampleButton.GetComponent<RectTransform>();
+
+		buttonRectTransform.anchorMax = new Vector2(1, 1);
+		buttonRectTransform.anchorMin = new Vector2(0, 0);
+
+		buttonRectTransform.offsetMin = new Vector2(buttonRectTransform.offsetMin.x, 0);
+		buttonRectTransform.offsetMax = new Vector2(buttonRectTransform.offsetMax.x, 0);
+		buttonRectTransform.offsetMin = new Vector2(buttonRectTransform.offsetMin.y, 0);
+		buttonRectTransform.offsetMax = new Vector2(buttonRectTransform.offsetMax.y, 0);
+	}
+
+	//сетапим все кнопки из currentItemList в цикле
+	public void AddButtonsFromCurrentItemList()
+	{
+		for (int i = 0; i < currentItemList.Count; i++) 
+		{
+			Item currentItem = currentItemList [i];
+			TakeOneButtonFromPoolAndSetupWith (currentItem);
+		}
+	}
+
+
+	public void TuneButtonsForMain ()
+	{
+        foreach (Transform mainButton in transform) 
+		{
+			mainButton.GetComponent <SampleButton> ().TuneButtonForMain ();
+		}
+	}
+
+	public void TuneButtonsForBrowse ()
+	{
+        foreach (Transform browseButton in transform) 
+		{
+			browseButton.GetComponent <SampleButton> ().TuneButtonForBrowse ();
+        }
+	}
+
+	public void TuneButtonsForQuiz ()
+	{
+		foreach (Transform quizButton in transform) 
+		{
+			quizButton.GetComponent <SampleButton> ().TuneButtonForQuiz ();
+		}
+	}
+
+    // из общего списка объектов отфильтруем тех, кто не объект, а категория объектов
+    public void FilterCategoryPickerItemList()
+	{
+		foreach (Item sortedItem in dataContainer.allItemsList) 
+		{
+			if (sortedItem.isACategory == true) 
+			{ 
+				categoryPickerButtonsController.currentItemList.Add (sortedItem);
+			}
+		}
+	}
+
+    // из общего списка объектов отфильтруем тех, кто принадлежит нужной категории
+    public void FilterObjectPickerItemListTo (int desiredCategoryId)
+	{
+		foreach (Item sortedItem in dataContainer.allItemsList)
+		{
+			if (sortedItem.Category == desiredCategoryId &&  sortedItem.isACategory ==false)
+			{
+				objectPickerButtonsController.currentItemList.Add (sortedItem);
+			}
+		}
+	}
+
+	//убрать в пул одну кнопку 
+	public void ReturnOneButtonToPool ()
+	{	
+		GameObject toBeRemoved = transform.GetChild (0).gameObject;
+		buttonObjectPool.ReturnObject (toBeRemoved);
+	}
+
+	//убирать в пул все кнопки  пока не кончатся
+	public void RemoveAllButtons() 
+	{
+		currentController = this;
+		while (currentController.transform.childCount > 0)
+        // а почему сразу не использовать this ?
+        // TODO разобраться
+        {
+            ReturnOneButtonToPool ();
+		}
+	}
+}
--- Assets/Scripts/SampleButton.cs
+++ Assets/Scripts/SampleButton.cs
@@ -1,4 +1,18 @@
-﻿using System.Collections;using System.Collections.Generic;using UnityEngine;using UnityEngine.UI;public class SampleButton : MonoBehaviour {	public Button button;	public GameObject letterPanel;	public GameObject namePanel;	public Text nameLabel;	public Text initialLetter;	public Image iconImage;	private Item item;	private bool isDislpayingPictureNow;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour 
+{
+	public Button button;
+	public GameObject letterPanel;
+	public GameObject namePanel;
+	public Text nameLabel;
+	public Text initialLetter;
+	public Image iconImage;
+	private Item item;
+	private bool isDislpayingPictureNow;
 	private AudioClip letterClip;
 	private AudioClip nameClip;
 	public SoundController soundController;
@@ -8,8 +22,29 @@
 		soundController = FindObjectOfType<SoundController>();
 	}
 
-	void Update()	{		if (transform.parent.name == "BrowseModePanel") 		{			if (Input.GetKeyUp("right"))			{				SwitchToNextSprite ();			}			if (Input.GetKeyUp("left"))			{				SwitchToPreviousSprite ();			}			}	}	public void Setup (Item itemToSetupWith)	{
-		item = itemToSetupWith;		nameLabel.text = item.itemName;		initialLetter.text = item.initialLetter.ToString ();		SetupSprite ();		button.gameObject.name = item.itemName;
+	void Update()
+	{
+		if (transform.parent.name == "BrowseModePanel") 
+		{
+			if (Input.GetKeyUp("right"))
+			{
+				SwitchToNextSprite ();
+			}
+
+			if (Input.GetKeyUp("left"))
+			{
+				SwitchToPreviousSprite ();
+			}	
+		}
+	}
+
+	public void Setup (Item itemToSetupWith)
+	{
+		item = itemToSetupWith;
+		nameLabel.text = item.itemName;
+		initialLetter.text = item.initialLetter.ToString ();
+		SetupSprite ();
+		button.gameObject.name = item.itemName;
 		AudioSource[] allAudioSourcesOfButton = item.GetComponents<AudioSource>();
 		if (allAudioSourcesOfButton.Length>0)
 		{
@@ -19,7 +54,88 @@
 		{
 			letterClip = allAudioSourcesOfButton[1].clip;
 		}
-	}	public void SetupSprite ()	{		if (item.spriteWasSelected == false) //если номер спрайта не был выбран ранее		{			// то выберем новый спрайт			int numberOfRandomPicture = Random.Range (0, item.pictureList.Capacity);						// отметим флажком, что у этого айтема спрайт уже выбран			item.spriteWasSelected = true;			//пропишем выбранный спрайт в айтем 			item.savedNumberOfSelectedPicture = numberOfRandomPicture;		} 		//пропишем выбранный спрайт в кнопку 		iconImage.sprite = item.pictureList [item.savedNumberOfSelectedPicture];	}	public void SwitchToNextSprite()	{		// Если выбран последний спрайт, то переключаем на первый, т.к. листаем по кругу		if(item.savedNumberOfSelectedPicture==(item.pictureList.Count-1))		{			item.savedNumberOfSelectedPicture = 0;		}        // Для всех остальных кроме последнего просто увеличим номер на единицу		else		{			item.savedNumberOfSelectedPicture++;					}		Setup (item); 	}	public void SwitchToPreviousSprite()	{        //если выбран первый спрайт, то переключим на последний, т.к. листаем по кругу        if (item.savedNumberOfSelectedPicture==0)		{			item.savedNumberOfSelectedPicture = (item.pictureList.Count-1);		}        // Для всех остальных кроме перого просто уменьшим номер на единицу        else        {			item.savedNumberOfSelectedPicture--;					}		Setup (item);	}	//обработка нажатий	public void HandleClick()	{        PanelsController panelsController = GameObject.Find ("MainCanvas").GetComponent<PanelsController> ();		QuizController quizController = GameObject.Find ("QuizController").GetComponent<QuizController> ();		//выясняем кто parent нажатой кнопки, реагируем соответственно		switch (transform.parent.name) 		{            //если нажатая кнопка находится в панели выбора категории		case("CategoryPickerPanel"):             // то меняем список объектов текущей категории в панели выбора объектов            panelsController.ChangeObjectPickerItemListCategoryTo (item.Category);			// и обновляем панель выбора объектов            panelsController.RepopulateObjectPicker (); 			soundController.TellButtonName(nameClip);			break;		case("ObjectPickerPanel"): 			if(item.gameObject.name == "Quiz") //12 кнопка quiz			{				panelsController.GoQuizMode (); 			}			else //1-11 кнопка			{				panelsController.ChangeBrowseModeItemListTo (item);				panelsController.GoBrowseMode ();				soundController.TellButtonName(nameClip);			}			break;		case("BrowseModePanel"):
+	}
+
+	public void SetupSprite ()
+	{
+		if (item.spriteWasSelected == false) //если номер спрайта не был выбран ранее
+		{
+			// то выберем новый спрайт
+			int numberOfRandomPicture = Random.Range (0, item.pictureList.Capacity);	
+		
+			// отметим флажком, что у этого айтема спрайт уже выбран
+			item.spriteWasSelected = true;
+
+			//пропишем выбранный спрайт в айтем 
+			item.savedNumberOfSelectedPicture = numberOfRandomPicture;
+		} 
+
+		//пропишем выбранный спрайт в кнопку 
+		iconImage.sprite = item.pictureList [item.savedNumberOfSelectedPicture];
+	}
+
+	public void SwitchToNextSprite()
+	{
+		// Если выбран последний спрайт, то переключаем на первый, т.к. листаем по кругу
+		if(item.savedNumberOfSelectedPicture==(item.pictureList.Count-1))
+		{
+			item.savedNumberOfSelectedPicture = 0;
+		}
+        // Для всех остальных кроме последнего просто увеличим номер на единицу
+		else
+		{
+			item.savedNumberOfSelectedPicture++;			
+		}
+		Setup (item); 
+	}
+
+	public void SwitchToPreviousSprite()
+	{
+        //если выбран первый спрайт, то переключим на последний, т.к. листаем по кругу
+        if (item.savedNumberOfSelectedPicture==0)
+		{
+			item.savedNumberOfSelectedPicture = (item.pictureList.Count-1);
+		}
+        // Для всех остальных кроме перого просто уменьшим номер на единицу
+        else
+        {
+			item.savedNumberOfSelectedPicture--;			
+		}
+		Setup (item);
+	}
+
+	//обработка нажатий
+	public void HandleClick()
+	{
+        PanelsController panelsController = GameObject.Find ("MainCanvas").GetComponent<PanelsController> ();
+		QuizController quizController = GameObject.Find ("QuizController").GetComponent<QuizController> ();
+
+		//выясняем кто parent нажатой кнопки, реагируем соответственно
+		switch (transform.parent.name) 
+		{
+            //если нажатая кнопка находится в панели выбора категории
+		case("CategoryPickerPanel"): 
+            // то меняем список объектов текущей категории в панели выбора объектов
+            panelsController.ChangeObjectPickerItemListCategoryTo (item.Category);
+			// и обновляем панель выбора объектов
+            panelsController.RepopulateObjectPicker (); 
+			soundController.TellButtonName(nameClip);
+			break;
+
+		case("ObjectPickerPanel"): 
+			if(item.gameObject.name == "Quiz") //12 кнопка quiz
+			{
+				panelsController.GoQuizMode (); 
+			}
+			else //1-11 кнопка
+			{
+				panelsController.ChangeBrowseModeItemListTo (item);
+				panelsController.GoBrowseMode ();
+				soundController.TellButtonName(nameClip);
+			}
+			break;
+
+		case("BrowseModePanel"):
 				if (isDislpayingPictureNow==true)
 				{
 					// если это первый экран для browseMode , то 
@@ -30,10 +146,77 @@
 					// и ставим флаг, сигнализирующий, что мы уже не на первом экране
 					isDislpayingPictureNow = false;
 					break;
-				}				else				{
+				}
+				else
+				{
 					// если это второй экран для browseMode , то возвращаемся на главный экран
 					panelsController.RefreshMainModeItemLists(item.Category);
 					panelsController.RandomizeObjectPickerSprites(); // TODO это должно быть не здесь
 					panelsController.GoMainMode();
-					break;				}		case("QuizButtonsPanel"):			bool guessWasSuccessfull = quizController.CheckIfWinner (item); 			if (guessWasSuccessfull == true) 			{				panelsController.ChangeBrowseModeItemListTo (item);				panelsController.GoBrowseMode ();			}			else 			{				this.MoveRedCrossForward (); //TODO CHECK IF THIS needed			}			break;		default:			Debug.LogError ("new or unknown category" + transform.parent.name);			break;		}	}    public void TuneButtonForPool()    {        namePanel.SetActive(false);        letterPanel.SetActive(false);        MoveRedCrossBackward();    }    public void TuneButtonForMain()	{		namePanel.SetActive (false);		letterPanel.SetActive (false);		MoveRedCrossBackward ();	}	public void TuneButtonForBrowse()	{		namePanel.SetActive (false);		letterPanel.SetActive (false);		MoveRedCrossBackward ();
-		isDislpayingPictureNow = true;    }	public void TuneButtonForQuiz()	{		namePanel.SetActive (false);		letterPanel.SetActive (false);		MoveRedCrossBackward ();	}	public void MoveRedCrossForward()	{		this.transform.Find ("WrongImage").SetAsLastSibling (); 	}	public void MoveRedCrossBackward()	{		this.transform.Find ("WrongImage").SetAsFirstSibling (); 	}	private void DisplayInitialLetterFullscreeen()	{		namePanel.SetActive(true);		letterPanel.SetActive(true);	}}
+					break;
+				}
+
+		case("QuizButtonsPanel"):
+			bool guessWasSuccessfull = quizController.CheckIfWinner (item); 
+			if (guessWasSuccessfull == true) 
+			{
+				panelsController.ChangeBrowseModeItemListTo (item);
+				panelsController.GoBrowseMode ();
+			}
+			else 
+			{
+				this.MoveRedCrossForward (); //TODO CHECK IF THIS needed
+			}
+			break;
+
+		default:
+			Debug.LogError ("new or unknown category" + transform.parent.name);
+			break;
+		}
+	}
+
+    public void TuneButtonForPool()
+    {
+        namePanel.SetActive(false);
+        letterPanel.SetActive(false);
+        MoveRedCrossBackward();
+    }
+
+    public void TuneButtonForMain()
+	{
+		namePanel.SetActive (false);
+		letterPanel.SetActive (false);
+		MoveRedCrossBackward ();
+	}
+
+	public void TuneButtonForBrowse()
+	{
+		namePanel.SetActive (false);
+		letterPanel.SetActive (false);
+		MoveRedCrossBackward ();
+		isDislpayingPictureNow = true;
+    }
+
+	public void TuneButtonForQuiz()
+	{
+		namePanel.SetActive (false);
+		letterPanel.SetActive (false);
+		MoveRedCrossBackward ();
+	}
+
+	public void MoveRedCrossForward()
+	{
+		this.transform.Find ("WrongImage").SetAsLastSibling (); 
+	}
+
+	public void MoveRedCrossBackward()
+	{
+		this.transform.Find ("WrongImage").SetAsFirstSibling (); 
+	}
+
+	private void DisplayInitialLetterFullscreeen()
+	{
+		namePanel.SetActive(true);
+		letterPanel.SetActive(true);
+	}
+}
