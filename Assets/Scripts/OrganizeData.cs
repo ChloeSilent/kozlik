1c4e91c3ae6729f639340f3597704a8640edc6aa
Andrey Martianov <v-osnovnom-bezvreden@yandex.ru>
2019-01-19 13:14:57 +0300

    рефакторинг

--- Assets/Scripts/OrganizeData.cs
+++ Assets/Scripts/OrganizeData.cs
@@ -1,24 +1,24 @@
 ﻿using System.Collections;
 using UnityEngine;
-using System.Collections.Generic; // нужно для  [System.Serializable]
+using System.Collections.Generic;
 
-[System.Serializable] // выводит в инспектор
 public class OrganizeData : MonoBehaviour 
 {
 	public List<Item> allItemsList;
 	public List<Item> categoriesItemsList;
 	public List<Item> itemsOfCategory;
-	public PanelsController panelsController;
+	private PanelsController panelsController;
 
 	void Start()
 	{
 		StartCoroutine("PrepareData");
-		//start only when data is ready
+		//корутина отработала, стартуем
+		panelsController = FindObjectOfType<PanelsController>();
 		panelsController.DelayedStart (); 
 	}
 
-    //декларация корутины
-    IEnumerator PrepareData()
+	//корутина готовит данные, когда она закончит работу - можно стартовать
+	IEnumerator PrepareData()
 	{
 		LoadAllDataToList ();
 		FindCategories ();
@@ -32,6 +32,7 @@ public class OrganizeData : MonoBehaviour
 		this.GetComponentsInChildren <Item> (allItemsList); 
 	}
 
+	// соберем категории в отдельный лист
 	public void FindCategories()
 	{
 		for (int i = 0; i < allItemsList.Count; i++)
@@ -43,26 +44,34 @@ public class OrganizeData : MonoBehaviour
 		}
 	}
 
+	// выберем  случайный спрайт для всей категории среди спрайтов предметов принадлежащих этой категории
 	public void GenerateCategorySprites()
 	{
 		for (int c = 0; c < categoriesItemsList.Count; c++)
 		{
-			categoriesItemsList[c].GetComponentsInChildren <Item> (itemsOfCategory); //all items of category
-			itemsOfCategory.RemoveAt (0); //kill category item itself
-			itemsOfCategory.RemoveAt (itemsOfCategory.Count-1); //kill quiz
+			// сначала соберем в один лист все предметы, принадлежащие данной категории
+			categoriesItemsList[c].GetComponentsInChildren <Item> (itemsOfCategory); 
 
-			int r = Random.Range (0, itemsOfCategory.Count); //index of random item of category 
+			// первым идёт сама категория, уберем её
+			itemsOfCategory.RemoveAt (0); 
+
+			// последним идёт предмет-квиз, уберем его тоже
+			itemsOfCategory.RemoveAt (itemsOfCategory.Count-1); 
+
+			// теперь в листе только преметы, принадлежащие данной категории, выберем из них случайный
+			int r = Random.Range (0, itemsOfCategory.Count); 
+
+			// у предмета из шага выше выберем рандомный спрайт и назначим его спрайтом для всей категории
 			categoriesItemsList [c].pictureList [0] = itemsOfCategory [r].pictureList [Random.Range (0, itemsOfCategory [r].pictureList.Count)];
 		}
 	}
 
+	//вычленим первую букву из названия
 	public void GenerateInitialLetter()
 	{
 		foreach (Item item in allItemsList)
 		{
 			item.initialLetter = item.itemName [0]; 
 		}
-
 	}
-
 }
