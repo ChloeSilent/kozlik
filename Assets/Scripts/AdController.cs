c337e6be4ffc067611fd7c81d18fddf8e8def60d
Andrey Martianov <v-osnovnom-bezvreden@yandex.ru>
2019-02-22 23:15:40 +0300

    доработки по рекламе  и тест покупки

--- Assets/Scripts/AdController.cs
+++ Assets/Scripts/AdController.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Monetization;
+//using UnityEngine.Monetization;
 using UnityEngine.Advertisements;
+using UnityEngine.UI;
 
 public class AdController : MonoBehaviour
 {
 	public int delayBeforeAds = 10;
 	public string placementId = "top_banner";
 	public bool adsEnabled = false;
+	public Text adStatusText;
+	public bool successfullyPurchased = false;
 
 	#if UNITY_ANDROID
 		public const string gameId = "3038717";
@@ -26,12 +29,18 @@ public class AdController : MonoBehaviour
 		Advertisement.Initialize(gameId, testMode);
 	}
 
+	// если не куплен отказ от рекламы , и прошло некторое время со старта приложения, то включаем рекламу
 	void Update()
 	{
-		if (Time.realtimeSinceStartup > delayBeforeAds) 
+		if (Time.realtimeSinceStartup > delayBeforeAds && successfullyPurchased==false) 
 		{
 			adsEnabled = true;
 		}
+		else
+		{
+			adsEnabled = false;
+		}
+		TellAdStatus();
 	}
 	
 	IEnumerator ShowBannerWhenReady()
@@ -55,5 +64,24 @@ public class AdController : MonoBehaviour
 		{
 			StartCoroutine(ShowBannerWhenReady());
 		}
+	}
+
+	private void TellAdStatus()
+	{
+		if (adsEnabled == true)
+		{
+			adStatusText.text = "ads enabled";
+		}
+		else
+		{
+			adStatusText.text = "ads disabled";
+		}
+	}
+
+	public void DisableAdsForever()
+	{
+		Debug.Log("DisableAdsForever");
+		successfullyPurchased = true;
+		Advertisement.Banner.Hide();
 	}
 }
