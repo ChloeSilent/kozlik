8dcb090c4efbcb6837aae4fd18c9c485737db45c
Andrey Martianov <v-osnovnom-bezvreden@yandex.ru>
2019-03-10 07:42:53 +0300

    close  #44 #45

--- Assets/Scripts/Ads/GoogleMobileAdsScript.cs
+++ Assets/Scripts/Ads/GoogleMobileAdsScript.cs
@@ -6,9 +6,6 @@ using GoogleMobileAds.Api;
 
 public class GoogleMobileAdsScript : MonoBehaviour
 {
-	private BannerView bannerView;
-	public int delayBeforeAds;
-
 	public string currentAppId;
 	public string androidAppId;
 	public string iosAppId;
@@ -19,29 +16,22 @@ public class GoogleMobileAdsScript : MonoBehaviour
 	public string testAdUnitIdForIos;
 	public string realAdUnitIdForIos;
 
+	private AdRequest request;
+	private BannerView bannerView;
+
 	public bool adsEnabled = false;
 	public bool successfullyPurchased = false;
+	public int delayBeforeAds;
 
 	public void Start()
 	{
-		InitializeAds();
-	}
-	
-	void Update()
-	{
-		//если не куплен отказ от рекламы, и прошло некторое время со старта приложения, то включаем рекламу
-		if (Time.realtimeSinceStartup > delayBeforeAds && successfullyPurchased == false)
-		{
-			adsEnabled = true;
-		}
-		else
-		{
-			adsEnabled = false;
-		}
+		SetupIds();
+		CreateBannerView();
+		CreateAdRequest();
+		LoadRequestedBannerToView();
 	}
 
-
-	private void InitializeAds()
+	private void SetupIds()
 	{
 		#if UNITY_EDITOR
 			currentAppId = "no currentAppId  available for UNITY_EDITOR";
@@ -68,23 +58,49 @@ public class GoogleMobileAdsScript : MonoBehaviour
 		#endif
 	}
 
-	private void RequesAndDisplayBanner()
+	private void CreateBannerView()
 	{
-		// Create a 320x50 banner at the top of the screen.
-		bannerView = new BannerView(currentAdUnitId, AdSize.Banner, AdPosition.Top);
-		// Create an empty ad request.
-		AdRequest request = new AdRequest.Builder()
-		.TagForChildDirectedTreatment(true)
-		.Build();
-		// Load the banner with the request.
-		bannerView.LoadAd(request);
+		// Create a 320x50 banner view at the top of the screen.
+			bannerView = new BannerView(currentAdUnitId, AdSize.Banner, AdPosition.Top);
+			Debug.Log("ads: CreateBannerView called");
+	}
+
+	private void CreateAdRequest()
+	{
+			request = new AdRequest.Builder()
+			.AddTestDevice("31642AF81FC95C9F60ED1975BD0207B6") //mart
+			.AddTestDevice("4AC2811559563C8348D03C6D9DE61104") //son
+			.TagForChildDirectedTreatment(true)
+			.Build();
+			Debug.Log("ads: CreateAdRequest called");
+	}
+
+	private void LoadRequestedBannerToView()
+	{
+			bannerView.LoadAd(request);
+
+			Debug.Log("ads: LoadRequestedBannerToView called");
 	}
 
 	public void ShowAdsIfEnabled() 
 	{
+		// if delay from appstart has already passed AND user didnt acquire disabling ads, 
+		// then set  adsEnabled flag to true
+		if (Time.realtimeSinceStartup > delayBeforeAds && successfullyPurchased == false)
+		{
+			adsEnabled = true;
+			Debug.Log("ads: adsEnabled set to true");
+		}
+		else
+		{
+			adsEnabled = false;
+			Debug.Log("ads: adsEnabled set to false");
+		}
+
+		// show ads if flag allows, or do nothing if doesnt
 		if (adsEnabled == true)
 		{
-			this.RequesAndDisplayBanner();
+			bannerView.Show();
 		}
 	}
 
@@ -92,18 +108,21 @@ public class GoogleMobileAdsScript : MonoBehaviour
 	{
 		if (bannerView != null)
 		{
-			bannerView.Destroy();
+			bannerView.Hide();
+			Debug.Log(" ads: TemporarilyHideAds called");
 		}
 	}
 
 	public void DisableAdsForever()
 	{
-		this.TemporarilyHideAds();
+		if (bannerView != null)
+		{
+			bannerView.Destroy();
+		}
 		successfullyPurchased = true;
 		adsEnabled = false;
+		Debug.Log(" ads: DisableAdsForever called");
 	}
-
-
 }
 
 
